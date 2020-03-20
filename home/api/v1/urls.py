ea9b21446eecc1aac5ed67b2e57871e1575ba56b
Crowdbotics <team@crowdbotics.com>
2020-03-20 20:49:34 +0000

    Update models

--- home/api/v1/urls.py
+++ home/api/v1/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import CustomTextViewSet, HomePageViewSet
 
-from home.api.v1.viewsets import SignupViewSet, LoginViewSet, HomePageViewSet, CustomTextViewSet, AppReportView
+from home.api.v1.viewsets import (
+    SignupViewSet,
+    LoginViewSet,
+    HomePageViewSet,
+    CustomTextViewSet,
+    AppReportView,
+)
 
 router = DefaultRouter()
-router.register('signup', SignupViewSet, base_name='signup')
-router.register('login', LoginViewSet, base_name='login')
-router.register('customtext', CustomTextViewSet)
-router.register('homepage', HomePageViewSet)
+router.register("signup", SignupViewSet, base_name="signup")
+router.register("login", LoginViewSet, base_name="login")
+router.register("customtext", CustomTextViewSet)
+router.register("homepage", HomePageViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
